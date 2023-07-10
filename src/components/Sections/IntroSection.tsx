c5f4110e87fb8316ee9953c9fec05c2b124eab5d
kyle sexton <ksexton.exe@gmail.com>
2023-07-09 23:41:31 -0400

    tile whileHover and mediaQuery

--- src/components/Sections/IntroSection.tsx
+++ src/components/Sections/IntroSection.tsx
@@ -1,10 +1,16 @@
+import { useMediaQuery } from '@mui/material';
 import { HexPanel } from '../HexPanel';
 import { SectionContainer } from '../SectionContainer';
 
 export const IntroSection = () => {
+  const isMediumSizeScreen = useMediaQuery('(min-width:600px)');
+
   return (
     <SectionContainer>
-      <HexPanel numberOfRows={7} tilesPerRow={10} />
+      <HexPanel
+        numberOfRows={isMediumSizeScreen ? 7 : 9}
+        tilesPerRow={isMediumSizeScreen ? 10 : 6}
+      />
     </SectionContainer>
   );
 };
